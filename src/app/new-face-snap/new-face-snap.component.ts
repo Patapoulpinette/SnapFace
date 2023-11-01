c93d07b318581efeb9ac73faf3e50b0515c13e0a
Delphine <dbouron@student.42lyon.fr>
2023-11-01 21:27:06 +0100

    Add newFaceSnap reactive form with validators and observable for realtime preview in newFaceSnap page

---
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {map, Observable} from "rxjs";
+import {FaceSnapModel} from "../models/face-snap.model";
+
+@Component({
+  selector: 'app-new-face-snap',
+  templateUrl: './new-face-snap.component.html',
+  styleUrls: ['./new-face-snap.component.scss']
+})
+export class NewFaceSnapComponent implements OnInit {
+  snapForm!: FormGroup;
+  faceSnapPreview$!: Observable<FaceSnapModel>;
+  urlRegex!: RegExp;
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/;
+    this.snapForm = this.formBuilder.group({
+      title: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+      imageUrl: this.formBuilder.control(null, {
+        validators: [Validators.required, Validators.pattern(this.urlRegex)],
+        updateOn: 'blur'
+      }),
+      location: [null]
+    });
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map(formValue => ({
+        ...formValue,
+        createdDate: new Date(),
+        snaps: 0,
+        id: 0
+      }))
+    );
+  }
+
+  onSubmitForm() {
+    console.log(this.snapForm.value);
+  }
+}
