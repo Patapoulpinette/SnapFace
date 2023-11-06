3c6b52e15e07b1f0f4f2f63134c84483cf58273b
Delphine <dbouron@student.42lyon.fr>
2023-11-06 20:32:39 +0100

    Change static variables by Observables and http requests to get/put/post info from/to the backend

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FaceSnapsService} from "../services/face-snaps.service";
 import {FaceSnapModel} from "../models/face-snap.model";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-face-snap-list',
@@ -8,10 +9,10 @@ import {FaceSnapModel} from "../models/face-snap.model";
   styleUrls: ['./face-snap-list.component.scss']
 })
 export class FaceSnapListComponent implements OnInit {
-  faceSnaps!: FaceSnapModel[];
+  faceSnaps$!: Observable<FaceSnapModel[]>;
   constructor(private faceSnapsService: FaceSnapsService) {}
 
   ngOnInit(): void {
-    this.faceSnaps = this.faceSnapsService.faceSnaps;
+    this.faceSnaps$ = this.faceSnapsService.getAllFaceSnaps();
   }
 }
